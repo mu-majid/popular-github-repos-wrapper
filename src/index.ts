9f4a5998674f1a488a2068f914cd7a9666fb31dc
Muhammad Majid <mmajidkamel@gmail.com>
2020-11-23 19:37:17 +0200

    scaffold (Express + Typescript) project

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+
+import { popularReposRouter } from './routes/list-popular-repos';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found';
+
+
+// cookie here is for transporting jwt
+const app = express();
+app.use(json());
+
+
+app.use(popularReposRouter);
+
+app.all('*', async () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+app.listen(3000, () => {
+  console.log('Popular repos Server on port 3000!!!');
+});
